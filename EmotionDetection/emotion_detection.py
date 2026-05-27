09a248756a77644f470c50693cf5f2cdbe872da5
Thiri San <thirisan662003@gmail.com>
2026-05-27 06:51:54 -0400

    Packaged EmotionDetection with __init__.py

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    headers = {
+        "Content-Type": "application/json",
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+    payload = {"raw_document": {"text": text_to_analyze}}
+    
+    response = requests.post(url, headers=headers, json=payload)
+    
+    # Convert response text into dictionary
+    result = json.loads(response.text)
+    
+    # Extract emotions
+    emotions = result['emotionPredictions'][0]['emotion']
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+    
+    # Find dominant emotion
+    dominant_emotion = max(emotions, key=emotions.get)
+    
+    # Return formatted dictionary
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
